2fcb73abd6fb6a4fa7e11e9f95aab489a7b03eae
Kong M Vang <kongvang2@csus.edu>
2021-11-29 01:09:15 -0800

    Also added a csv to json converter to be used on the program. - Kong

---
+++ movie_demo/src/main/java/com/example/demo/CsvToJson.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+public class CsvToJson {
+
+    public static void run() throws Exception {
+        File input = new File("Kaggle_2020_the_oscar_award.csv");
+        File output = new File("Kaggle_2020_the_oscar_award.json");
+
+        List<Map<?, ?>> data = readObjectsFromCsv(input);
+        writeAsJson(data, output);
+    }
+
+    public static List<Map<?, ?>> readObjectsFromCsv(File file) throws IOException {
+        CsvSchema bootstrap = CsvSchema.emptySchema().withHeader();
+        CsvMapper csvMapper = new CsvMapper();
+        MappingIterator<Map<?, ?>> mappingIterator = csvMapper.reader(Map.class).with(bootstrap).readValues(file);
+
+        return mappingIterator.readAll();
+    }
+
+    public static void writeAsJson(List<Map<?, ?>> data, File file) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        mapper.writeValue(file, data);
+    }
+}
