4ab730ac0db1f8696d64563d89b93fc5439fc472
Kong M Vang <kongvang2@csus.edu>
2021-11-29 01:09:46 -0800

    updated

--- movie_demo/src/main/java/com/example/demo/CsvToJson.java
+++ movie_demo/src/main/java/com/example/demo/CsvToJson.java
@@ -7,7 +7,6 @@ import java.util.Map;
 
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
